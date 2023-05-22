98408be3736b260dc3312e2248d12b0844de2a2d
Bishawnath Ray <bishawnathprogram07@gmail.com>
2023-05-22 23:26:16 +0600

    added navber , banner , category and footer section

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className='btn btn-primary'>button</button>
+      <button className='btn btn-primary'>button</button>
+      <button className='btn btn-warning'>button</button>
+    </>
+  )
+}
+
+export default App
